0146fb4a6662ceb9e865fa428c5ec3c1afa91fed
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-10-01 21:55:20 +0300

    Job done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+  function superbowlWin(record) {
+    const winningYear = record.find(item => item.result === "W");
+  
+    if (winningYear) {
+      return winningYear.year
+    } else {
+      return undefined
+    }
+  }
+  
+
+  const yearOfWin = superbowlWin(record)
+  console.log(yearOfWin)
+  
